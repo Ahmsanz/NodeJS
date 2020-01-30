d10c1b07d1e8944153e66ab0b5f2cfcb1d65200e
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-01-30 17:22:40 +0100

    reading streams

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require ('http');
 
-var server = http.createServer(function(req, res){
-  res.writeHead (200, {'Content-Type': 'text/plain'})
-  res.end('Hey ninjas!');
-});
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+  console.log('new chunk received');
+  console.log(chunk);
+})
 
 
-server.listen(3000, '127.0.0.1');
-console.log('listening to port 3000');
-
-//server set up
+// var server = http.createServer(function(req, res){
+//   console.log('request was made: ' + req.url);
+//   res.writeHead (200, {'Content-Type': 'text/plain'})
+//   res.end('Hey ninjas!');
+// });
+//
+//
+// server.listen(3000, '127.0.0.1');
+// console.log('listening to port 3000');
