2a028655e9e1c389ec2fe789aa336120922cdaa9
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-01-30 17:48:30 +0100

    reading and writing streams

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
 
+var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+
 myReadStream.on('data', function(chunk){
   console.log('new chunk received');
+  myWriteStream.write(chunk);
   console.log(chunk);
-})
+});
 
 
 // var server = http.createServer(function(req, res){
