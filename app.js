b7ec9384937f82e85cac7ba499639a24d82afcb9
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-01-30 18:17:39 +0100

    simple routing using only nodeJS

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
   console.log('request was made: ' + req.url);
-  res.writeHead (200, {'Content-Type': 'application/json'})
+  if (req.url === '/home' || req.url === '/') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    fs.createReadStream( __dirname + '/index.html').pipe(res);
+  } else if (req.url === '/contact') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    fs.createReadStream( __dirname + '/contact.html').pipe(res);
+  } else if (req.url === '/api/ninjas'){
+    var ninjas = [{
+      name: 'Ryu',
+      age: 29
+    },
+    {
+      name: 'Yoshi',
+      age: 31
+    }];
 
-  var myObj = {
-    name: 'Ryu',
-    job: 'ninja',
-    age: '29'
-  };
-
-  res.end(JSON.stringify(myObj));
-
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(ninjas));
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.createReadStream( __dirname + '/404.html').pipe(res);
+  }
 });
 
 
