2e586cf88449961e6cc07d389fcc064923c8a927
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-01-31 16:24:01 +0100

    html served as views

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/contact', function(req, res){
-    res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
 });
 
-app.get('/profile/:id', function(req, res){
-  res.send('You requested to see a profile with the id of ' + req.params.id)
+app.get('/profile/:name', function(req, res){
+  var data = {
+    age: 31,
+    job: 'ninja',
+    hobbies: [ 'eating', 'fighting', 'fishing']
+  }
+res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
