7be52f906950a1b53dddd38e6040e317f4deac09
Ahmsanz <adrian.h.m.sanz@gmail.com>
2020-01-31 15:19:03 +0100

    ejs installed and set up as view engine

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-var http = require ('http');
 
-var fs = require('fs');
+var express = require('express');
 
+var app = express();
 
-var server = http.createServer(function(req, res){
-  console.log('request was made: ' + req.url);
-  if (req.url === '/home' || req.url === '/') {
-    res.writeHead(200, {'Content-type': 'text/html'});
-    fs.createReadStream( __dirname + '/index.html').pipe(res);
-  } else if (req.url === '/contact') {
-    res.writeHead(200, {'Content-type': 'text/html'});
-    fs.createReadStream( __dirname + '/contact.html').pipe(res);
-  } else if (req.url === '/api/ninjas'){
-    var ninjas = [{
-      name: 'Ryu',
-      age: 29
-    },
-    {
-      name: 'Yoshi',
-      age: 31
-    }];
+app.set('view engine', 'ejs');
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(ninjas));
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    fs.createReadStream( __dirname + '/404.html').pipe(res);
-  }
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/contact', function(req, res){
+    res.sendFile(__dirname + '/contact.html');
+});
 
-server.listen(3000, '127.0.0.1');
-console.log('listening to port 3000');
+app.get('/profile/:id', function(req, res){
+  res.send('You requested to see a profile with the id of ' + req.params.id)
+});
+
+app.listen(3000);
